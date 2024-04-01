27b7cd41346981262715cffed56f661048d98732
Eyas <148169791+Eyas-OB@users.noreply.github.com>
2024-04-01 15:06:42 -0500

    Add files via upload

---
+++ Class_Schedule.java
@@ -0,0 +1,20 @@
+
+public class Class_Schedule 
+{
+
+	public static void main(String[] args) 
+	{
+		
+		
+		
+		//COMPSCI-251: Intermediate Computer Programming (3 Credits)
+		//Lecture 401 10:00 - 10:50am MW EMS W190
+		
+		System.out.print("COMPSCI-251: Intermediate Computer Programming (3 Credits)");
+		System.out.println();
+		System.out.print("Lecture 401 10:00-10:50am MW EMS W190");
+
+		
+	}
+
+}
